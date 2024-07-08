0d9dd88ad0f31e89724cf325d38e129fc405cf20
sadiakanwal10 <sadiakanwal715@gmail.com>
2024-07-08 05:44:21 -0700

    first commit

---
+++ main.ts
@@ -0,0 +1,8 @@
+// SYNTAX ERROR //
+// har language ka kuch nah kuch syntax hota hai jisy follow krna zaruri hota hai agar hum usy follow nae krengay
+// ya kuch spelling mistake krengay tw syntax error ayega.
+
+
+lett message = "HELLO WORLD";
+console.log(message);
+
